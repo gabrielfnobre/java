6b34bdbabcfc37e59784590e12036ff9a9640515
Gabriel-F-Nobre <nobrecoder@gmail.com>
2022-07-29 19:02:27 -0300

    29/07/2022

---
+++ CONSULTA_RAPIDA_ARQUIVOS/Integer.java
@@ -0,0 +1,19 @@
+//INTEGER
+
+//When we want to use integers Java we must use "int" as variable prefix...
+
+
+public class Integer {
+
+    public static void main (String[] args){
+
+        //See here the "int" being used
+        int number = 0;
+
+        number += 3;
+
+        System.out.println("The number zero added to three gives: " + number);
+    
+    }
+
+}
