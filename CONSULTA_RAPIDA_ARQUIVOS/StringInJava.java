9a9b7ea28870586f33bb77ba38d101431a166aad
Gabriel-F-Nobre <nobrecoder@gmail.com>
2022-08-04 18:11:22 -0300

    04/08/2022

---
+++ CONSULTA_RAPIDA_ARQUIVOS/StringInJava.java
@@ -0,0 +1,17 @@
+//STRING IN JAVA
+
+//In Java we can also use strings, in fact there is no primitive type "string" in Java,  
+//strings in Java are an address to a memory space that stores a set characters. 
+
+
+public class StringInJava {
+
+    public static void main(String[] args){
+
+        //See, strings always be written with double quotes, and the word "string" starts with a capital letter
+        String word = "word";
+        System.out.println(word); // => word
+
+    }
+
+}
