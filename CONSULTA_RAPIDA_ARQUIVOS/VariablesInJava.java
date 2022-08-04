854bb7acf1903537f7bc3d4f9df8ff6c25300fad
Gabriel-F-Nobre <nobrecoder@gmail.com>
2022-08-04 18:39:22 -0300

    04/08/2022

---
+++ CONSULTA_RAPIDA_ARQUIVOS/VariablesInJava.java
@@ -0,0 +1,22 @@
+//VARIABLES IN JAVA
+
+//In Java we can use variables without fear that a referece to the memory space of the assigned variable 
+//will be passed to the receiving variable. The patern in Java is, that a variable that receives another 
+//variable, takes only its values.
+
+
+public class VariablesInJava {
+
+    public static void main(String[] args){
+
+        //See that the variable second takes only the value of the variable first...
+        int first = 5;
+        int second = first;
+        System.out.println(second); // => 5 | the variable takes only the value...
+
+        first = 10; //even if another value is assigned in the variable "first"...
+        System.out.println(second); // => 5 | the value in "second" is not changed...
+
+    }
+
+}
