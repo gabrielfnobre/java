b22e5c16c10be68e1d23c2e9281a89d031014415
Gabriel-F-Nobre <nobrecoder@gmail.com>
2022-08-05 19:33:05 -0300

    05/08/2022

---
+++ CONSULTA_RAPIDA_ARQUIVOS/BolleansOperators.java
@@ -0,0 +1,32 @@
+//BOOLEANS OPERATORS
+
+//In the Java there are also booleans operators, to test with two or more different situations.
+//The booleans operators are:
+//      && - Operator AND
+//      || - Operator OR
+//      !  - Operator NOT
+//      == - Operator EQUAL
+//      === - Opertator EXTRICT EQUAL
+
+
+public class BolleansOperators {
+
+    public static void main(String[] args){
+
+        //Let's test booleans operators...
+        boolean truth = true;
+        boolean lie = false;
+
+        if(truth && lie){
+            System.out.println(truth && lie); // => | Nothing, because true and fause don't generate "true"
+        } 
+        if(truth || lie){
+            System.out.println(truth || lie); // => true
+        } 
+        if(truth == !lie){ //Here we are using the equal operator along with the negation...
+            System.out.println(truth); // => true
+        }
+
+    }
+
+}
---
+++ CONSULTA_RAPIDA_ARQUIVOS/Booleans.java
@@ -0,0 +1,21 @@
+//BOOLEANS
+
+//Booleans are a primitive type that use only "true" and "false" values, or they can takes boolean expressions
+//Booleans do not accept strings written as "true", or "int" with value 1 or 0, in Java booleans must always 
+//get "true" ou "false"
+
+
+public class Booleans {
+
+    public static void main(String[] args){
+
+        //Let's test booleans...
+        boolean value = true; // booleans receive a true or false
+        boolean valueFalse = (!false) != (!true); // but, too receive booleans expressions
+
+        System.out.println(value); // => true
+        System.out.println(valueFalse); // => true
+
+    }
+
+}
