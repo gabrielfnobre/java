46d64933092ac34a4023e08dfdea8d27ad7f6c43
Gabriel-F-Nobre <nobrecoder@gmail.com>
2023-02-18 17:02:39 -0300

    18/02/2023

---
+++ CONSULTA_RAPIDA_ARQUIVOS/PraticeTests.java
@@ -0,0 +1,9 @@
+public class PraticeTests {
+
+    public static void main(String[] args){
+
+        System.out.println("\n\tThis is a simple test using arguments as: " + args[0]  + " and " + args[1]); // => The exit would be the two arguments used for you...
+
+    }
+
+}
---
+++ PROJECTS/LIBRARY/src/BooksCalculator.java
@@ -0,0 +1,7 @@
+public class BooksCalculator {
+    public static void main(String[] args){
+        double bookJava8 = 59.90;
+        double bookTDD = 59.90;
+        double sumOfBooks = bookJava8 + bookTDD;
+    }
+}
