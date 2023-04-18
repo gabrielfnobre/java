91ad576cc6257af4f4f945c864a30af753ba7b7d
Gabriel <nobrecoder@gmail.com>
2023-04-18 12:06:43 -0300

    18/04/2023

---
+++ FUNDAMENTALS/Fundamentals of Java/Sentences/Sentences.java
@@ -0,0 +1,24 @@
+//Sentenças
+
+/*  Uma sentença de código é um um trecho executável de um código. Podemos compará-lo a uma frase em um parágrafo.
+    O que delimita uma frase é que ela tem um começo e termina com alguma sinalização como "." ou "!".
+    O código funciona da mesma forma, ele é uma sequência de sentenças, onde cara uma finaliza com ";".
+    Isso sigfica que uma sentença num código pode ter desde 1 única linha, até diversas linha, você quem decide.
+    Isso é legal, por que significa que podemos organizar o nosso código da maneira que for mais inteligível para 
+    nós e para os outros.
+    Veja um exemplo abaixo...
+
+    OBSERVAÇÃO: É claro que algumas coisas não podem ser separadas em várias linhas, por exemplo as strings dentro 
+    de um método println não podem ser separadas em várias linhas.
+ */
+
+public class Sentences {
+    public static void main(String[] args) {
+        //Em vez de escrever "System.out.println" em uma única linha, nós separamos em várias, pois o que 
+        //delimita uma sentença é ";" e não o número de linhas.
+        System.
+        out.
+        println(
+            "Funciona mesmo que separado em várias linhas");
+    }
+}
