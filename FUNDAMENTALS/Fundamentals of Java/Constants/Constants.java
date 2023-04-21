a376e7a4aa4d0c07d91c6d040fe593c32fd338a6
Gabriel-F-Nobre <nobrecoder@gmail.com>
2023-04-21 16:09:44 -0300

    21/04/2023

---
+++ FUNDAMENTALS/Fundamentals of Java/Constants/Constants.java
@@ -0,0 +1,49 @@
+//CONSTANTES:
+
+/*  As constantes em Java, assim como em todas as demais linguagens, armazenam
+    valores que não poderão ser mudados. As constantes em Java tem suas 
+    particularidades. Por exemplo:
+
+        * USO DO "FINAL": Toda constante em Java deve começar com a palavra 
+                reservada "final". Isso quer dizer que aquela variável é uma
+                variável que já recebeu o seu valor "final", portando não 
+                poderá ter o seu valor reatribuído;    
+
+        * FORTEMENTE TIPADAS: Uma constante em Java deve ter o seu tipo 
+                declarado obrigatóriamente após a declaração da palavra 
+                reservada "final" e antes da inicialização da variável;
+
+    Veja o exemplo abaixo...
+ 
+ */
+
+public class Constants {
+    public static void main(String[] args){
+        
+        //Aqui temos uma variável do tipo double
+        double raio = 3.4;
+
+        //Aqui, não é uma variável, e sim uma constante, constantes no Java
+        //recebem o "final" indicando que esse valor é o final e não poderá
+        //ser trocado.
+        final double pi = 3.14159;
+        double area = pi * raio * raio;
+
+        //Note que o valor do primeiro resultado funciona conforme o 
+        //planejado.
+        System.out.println("Primeiro resultado: " + area);
+
+        //Mas note que fizemos uma troca no valor do raio...
+        raio = 4;
+        //Embora o valor tenha sido trocado, como estamos chamando a 
+        //variável "area" que foi executada antes de termos feito a troca
+        //do valor, ela não vai aderir ao valor novo.
+        System.out.println("Segundo resultado: " + area);
+
+        //Porém se fizermos agora a mesma conta da circunferência após a
+        //troca do valor do raio, o valor da circunferência mudará.
+        area = pi * raio * raio;
+        System.out.println("Terceiro resultado: " + area);
+
+    }
+}
---
+++ FUNDAMENTALS/Fundamentals of Java/Variables/Variables.java
@@ -0,0 +1,52 @@
+//VARIAVEIS:
+
+/*  As variáveis em Java, assim como em todas as demais linguagens, armazenam
+    valores no espaço de memória de um computador. Mas é importante reforçar 
+    que no Java as variáveis tem particularidades especiais. Por exemplo:
+
+        * FORTEMENTE TIPADAS: Uma variável em Java deve ter o seu tipo 
+                declarado obrigatóriamente antes da inicialização da variável.
+                E NÃO PODEM MAIS MUDAR o seu tipo;
+
+        * ARMAZENAM VALORES CONFORME O TEMPO DE EXECUÇÃO: Quando uma variável
+                tem o seu valor trocado no decorrer da execução de um
+                algoritmo, todas as linhas anteriores que fizerem referência 
+                àquela variável antes do valor dela ter sido trocado, vão 
+                operar sobre o valor antigo.
+                Operações sobre o valor novo só valerão para linhas de código
+                que estiverem após a troca do valor;
+
+    Veja o exemplo abaixo...
+ 
+ */
+
+public class Variables {
+    public static void main(String[] args){
+        
+        //Aqui temos uma variável do tipo double
+        double raio = 3.4;
+
+        //Aqui, não é uma variável, e sim uma constante, constantes no Java
+        //recebem o "final" indicando que esse valor é o final e não poderá
+        //ser trocado.
+        final double pi = 3.14159;
+        double area = pi * raio * raio;
+
+        //Note que o valor do primeiro resultado funciona conforme o 
+        //planejado.
+        System.out.println("Primeiro resultado: " + area);
+
+        //Mas note que fizemos uma troca no valor do raio...
+        raio = 4;
+        //Embora o valor tenha sido trocado, como estamos chamando a 
+        //variável "area" que foi executada antes de termos feito a troca
+        //do valor, ela não vai aderir ao valor novo.
+        System.out.println("Segundo resultado: " + area);
+
+        //Porém se fizermos agora a mesma conta da circunferência após a
+        //troca do valor do raio, o valor da circunferência mudará.
+        area = pi * raio * raio;
+        System.out.println("Terceiro resultado: " + area);
+
+    }
+}
