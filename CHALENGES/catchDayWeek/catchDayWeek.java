a375730952d94b6dc30e9dfab9c721f1e35fb508
Gabriel <nobrecoder@gmail.com>
2023-05-10 05:57:12 -0300

    10/05/2023

--- CHALENGES/catchDayWeek/catchDayWeek.java
+++ CHALENGES/catchDayWeek/catchDayWeek.java
@@ -10,23 +10,23 @@ public class catchDayWeek {
 
     public static void main(String[] args){
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o nome de um dia da semana (Sem acento, maiusculas ou caracteres especiais):");
-        String day = sc.nextLine();
+        Scanner sc = new Scanner(System.in, "UTF-8");
+        System.out.println("Digite o nome de um dia da semana (Sem acento ou caracteres especiais):");
+        String day = sc.next();
 
-        if(day.equals("domingo")){
+        if(day.equalsIgnoreCase("domingo")){
             System.out.println(day + " corresponde ao dia: 1");
-        } else if (day.equals("segunda")){
+        } else if (day.equalsIgnoreCase("segunda")){
             System.out.println(day + " corresponde ao dia: 2");
-        } else if (day.equals("terca")){
+        } else if (day.equalsIgnoreCase("terca")){
             System.out.println(day + " corresponde ao dia: 3");
-        } else if (day.equals("quarta")){
+        } else if (day.equalsIgnoreCase("quarta")){
             System.out.println(day + " corresponde ao dia: 4");
-        } else if (day.equals("quinta")){
+        } else if (day.equalsIgnoreCase("quinta")){
             System.out.println(day + " corresponde ao dia: 5");
-        } else if (day.equals("sexta")){
+        } else if (day.equalsIgnoreCase("sexta")){
             System.out.println(day + " corresponde ao dia: 6");
-        } else if (day.equals("sabado")){
+        } else if (day.equalsIgnoreCase("sabado")){
             System.out.println(day + " corresponde ao dia: 7");
         }
 
