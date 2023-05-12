7b9bcd858938916b715bf8a201fd637857b15f5e
Gabriel <nobrecoder@gmail.com>
2023-05-12 05:45:17 -0300

    12/05/2023

---
+++ CHALENGES/forDescending/ForDescending.java
@@ -0,0 +1,14 @@
+/*  Nesse chalenge:
+
+    O desafio e criar uma laço for onde eu posso decrementar o valor 10 de 2 em 2 até chegar em zero.
+ */
+
+public class ForDescending {
+    public static void main(String[] args){
+        
+        for(int i = 10; i >= 0; i -= 2){
+            System.out.println(i);
+        }
+
+    }
+}
