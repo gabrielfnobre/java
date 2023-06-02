f977dd5a57dcd17c7aa10da1fe825219b286c76d
Gabriel <nobrecoder@gmail.com>
2023-06-02 05:51:11 -0300

    02/06/2023

---
+++ CHALENGES/ArraysGradesOfStudent/ArraysGradesOfStudent.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import javax.swing.JOptionPane;
+
+public class ArraysGradesOfStudent {
+    public static void main(String[] args){
+        
+        int numberGrades = Integer.parseInt(JOptionPane.showInputDialog("Quantas notas do aluno voce deseja colocar?"));
+        double[] grades = new double[numberGrades];
+        double averableGrade = 0;
+        String finalMesage = "As notas do aluno foram:\n";
+
+        for(int i = 0; i < grades.length; i++){
+            grades[i] = Integer.parseInt(JOptionPane.showInputDialog(String.format("Qual a %d nota?", i + 1)));
+        }
+
+        for(int i = 0; i < grades.length; i++){
+            averableGrade += grades[i];
+            finalMesage += String.format("\t%.1f\n", grades[i]);
+        }
+
+        averableGrade = averableGrade / numberGrades;
+
+        JOptionPane.showMessageDialog(null, String.format("%s\nTotalizando uma media de: %.1f", finalMesage, averableGrade));
+        System.exit(0);
+
+    }
+
+}
