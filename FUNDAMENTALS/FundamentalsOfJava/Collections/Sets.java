4186a94b1abdaaf1c128594df191bf6af8102c8e
Gabriel <nobrecoder@gmail.com>
2023-06-05 09:39:57 -0300

    05/06/2023

---
+++ FUNDAMENTALS/FundamentalsOfJava/Collections/Sets.java
@@ -0,0 +1,24 @@
+//  SETS:
+
+/*  O "set" é uma estrutura de dados dentro dos collections onde é possível armazenar valores das seguintes
+    formas:
+
+        # Heterogênea ou Homogênea:
+            Podemos armazenar valores que possuem tanto o mesmo tipo de dado como valores com tipos de 
+            dados diferente.
+            OBSERVAÇÃO: É preferível que os valores não sejam heterogêneos, não é visto como uma boa 
+            prática guardar valores heterogêneos num set, num list, num array ou qualquer estrutura que 
+            seja. Mas claro podem existir suas excessões, mas via de regra, não é uma boa prática;
+
+        # Não aceita valores duplicados:
+            O set não aceita valores iguais, apenas valores diferentes;
+
+        # Pode ser ordenado:
+
+        # Não pode ser indexado:
+            Diferente dos arrays e lists, o set não pode ser indexado;
+ */
+
+public class Sets {
+    
+}
